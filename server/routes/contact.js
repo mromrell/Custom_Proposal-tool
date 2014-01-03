58d135069a1e9673731221b9a68021394dded89b
Daniel Stephenson <dan.stephenson@gmail.com>
2014-01-02 17:41:46 -0700

    Pulled out a dummy route in contact.js (Node) because the controller doesn't exist.  Also cleaned up some debug information from a couple of .jade files.

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -3,5 +3,5 @@ module.exports = function(app, auth, contact) {
 //    app.get('/api/contact/:id', auth, contact.contact);
     app.post('/api/contact', auth, contact.addContact);
 //    app.put('/api/contact/:id', contact.editPost);
-    app.delete('/api/post/:id', contact.deletePost);
+//    app.delete('/api/post/:id', contact.deletePost);
 }

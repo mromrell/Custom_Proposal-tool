073ba07d71656ffa0aafadc8d0f8806e1ebb39bf
Skyler Osborn <skyler.osborn@gmail.com>
2014-01-15 11:29:17 -0700

    Current user dashboard name insert

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -216,6 +216,8 @@ angular.module('proposalTool.controllers', [])
             };
 
         $scope.proposal = SessionService.getCurrentProposal();
+        $scope.currentUserInfo = SessionService.getUserSession();
+        $scope.
     }])
 
     .controller('AddProposalController', ['$scope', '$window', 'proposalConstants', 'Restangular', 'SessionService', function($scope, $window, proposalConstants, Restangular, SessionService) {
